0eefc3373f632f7191f1489ef85a4dc79dedb523
aldoniq <aldiyar.yerzhanuli@gmail.com>
2019-06-30 15:48:26 +0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Пригласительный сайт</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('/vendor/bootstrap/css/bootstrap.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<!--  -->
+	<div class="simpleslide100">
+		<div class="simpleslide100-item bg-img1" style="background-image: url('images/bg01.jpg');"></div>
+		<div class="simpleslide100-item bg-img1" style="background-image: url('images/bg02.jpg');"></div>
+		<div class="simpleslide100-item bg-img1" style="background-image: url('images/bg03.jpg');"></div>
+		<div class="simpleslide100-item bg-img1" style="background-image: url('images/bg04.jpg');"></div>
+	</div>
+
+	<div class="flex-col-c-sb size1 overlay1 p-l-75 p-r-75 p-t-20 p-b-40 p-lr-15-sm">
+		<!--  -->
+		<div class="w-full flex-w flex-sb-m">
+			
+		</div>
+
+		<!--  -->
+		<div class="flex-col-c-m p-l-15 p-r-15 p-t-80 p-b-90">
+				
+			<h3 class="l1-txt2 txt-center p-b-55 respon1">
+				Askar&Altinay
+			</h3>
+			<div class="">
+					<script src="//megatimer.ru/get/2610ec64008de3ff76dad6b65f9d66a0.js"></script>
+				</div>
+
+		</div>
+		
+		
+
+				
+		</div>
+	</div>
+
+
+
+	
+
+</body>
+</html>
